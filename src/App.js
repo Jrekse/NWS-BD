b9cbb75c52593420076133b51033920041528c27
Jon Ekse <jrekse@gmail.com>
2021-09-21 08:11:04 -0700

    set the app up for prod

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Vss from "./assets/vssbannerhead.jpeg"
+import './App.css';
+
+class App extends Component {
+  // state = {
+  //   currentPage: "Home"
+  // };
+
+  // handlePageChange = page => {
+  //   this.setState({ currentPage: page });
+  // };
+
+  // renderPage = () => {
+  //   if (this.state.currentPage === "Home") {
+  //     return ;
+  //   } else if (this.state.currentPage === "Category") {
+  //     return ;
+  //   } else if (this.state.currentPage === "Post") {
+  //     return ; 
+  //   } else {
+  //     return 
+  //   }
+  // };
+  render(){
+      return (
+        <div className="App">
+          {/* {this.renderPage()} */}
+        </div>
+      )
+  }
+}
+
+export default App;
