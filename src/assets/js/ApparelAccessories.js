7025b4723a5fdbd0b07d2659f4a850395ecc505b
Jon Ekse <jrekse@gmail.com>
2021-10-07 10:05:36 -0700

    Scraped data from the business directory, seperated into renamed, repurposed js files as array data

---
+++ src/assets/js/ApparelAccessories.js
@@ -0,0 +1,17 @@
+const ApparelAccessories = [
+    {
+        'title' : 'NOMAR LLC',
+        'logo' : '',
+        'contact' : [
+                {   
+                    'phone' : '(907)-253-8363',
+                    'address' : '',
+                    'url' : 'https://nomaralaska.com/',
+                    'email' : 'info@nomaralaska.com'
+                }
+        ],
+        'content' : 'NOMAR creates and manufactures tough outerwear clothing and gear in Homer Alaska USA. Our Alaskan customers know what they need to stand up to the extreme weather of our state. Over the years they have helped us dial in on unique items that will last and work. Shop our retail outlet in Homer or our web store. wwwnomaralaska.com'
+    
+    },
+]
+export default ApparelAccessories;
---
+++ src/assets/js/Auctioneers.js
@@ -0,0 +1,17 @@
+const Auctioneers = [
+    {
+        'title' : 'James G. Murphy Co.',
+        'logo' : 'https://nwsportsmanmag.com/wp-content/uploads/2021/05/JamesGMurphy-logo.jpg',
+        'contact' : [
+                {   
+                    'phone' : '(425)-486-1246',
+                    'address' : '',
+                    'url' : 'https://murphyauction.com/',
+                    'email' : 'INFO@murphyauction.com'
+                }
+        ],
+        'content' : 'Northwest leader for Commercial and Industrial auctions. Conducting over 100 auctions annually, while building knowledge and integrity since 1970.'
+    
+    },
+]
+export default Auctioneers;
