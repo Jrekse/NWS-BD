839547e0dfec8980a081a008beb0994e364d6fb8
Jon Ekse <jrekse@gmail.com>
2021-10-11 08:31:30 -0700

    fixed the data.map to reflect changes made going from VSS to BD

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,54 +1,26 @@
 import React, {Component} from 'react';
-import CatData from '../assets/js/CategoryData';
-// import Data2 from '../assets/js/CategoryData2';
-// import Data3 from '../assets/js/CategoryData3';
+// import CatData from '../assets/js/Auctioneers.js';
+// import CatData from '../assets/js/ApparelAccessories.js';
+import CatData from '../assets/js/Boats:repair:outboards.js';
 import "../assets/css/Category.css";
 
-class Card extends Component{
+
+
+class Card extends Component{ 
     render(){
         return(
             <section className='categoryTable'>
                 {CatData.map((obj, index) => {
                     return(  
-                        <div className='cards'>            
-                            <h2>{obj.title}</h2>     
-                            <br/>
-                            <br/>
-                            <div className='PostCard' id={index}>
-                                <h2 className='title' >{obj.Posts[0].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[0].info}</p>
-                                </section>
-                            </div>
-                            <div className='PostCard' id={index + 1}>
-                                <h2 className='title' >{obj.Posts[1].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[1].info}</p>
-                                </section>
-                            </div>
-                            <div className='PostCard' id={index + 2}>
-                                <h2 className='title' >{obj.Posts[2].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[2].info}</p>
-                                </section>
-                            </div>
-                            <br/>
-                            <div className='PostCard' id={index + 3}>
-                                <h2 className='title' >{obj.Posts[3].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[3].info}</p>
-                                </section>
-                            </div>
-                            <div className='PostCard' id={index + 4}>
-                                <h2 className='title' >{obj.Posts[4].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[4].info}</p>
-                                </section>
-                            </div>
-                            <div className='PostCard' id={index + 5}>
-                                <h2 className='title' >{obj.Posts[5].title}</h2>
-                                <section className='Box'>
-                                    <p className='description'>{obj.Posts[5].info}</p>
+                        <div className='cards' key={index}>   
+                            <div className='PostCard'>
+                                <img src={obj.logo} alt='logo'/>
+                                <h2 className='title' >{obj.title}</h2>
+                                <section id="myDropdown" className="dropdown-content">
+                                    <p>{obj.phone}</p>
+                                    <p id='email'>{obj.email}</p>
+                                    <p><a href={obj.url}>Visit Our Website</a></p>
+                                    <p className='description'>{obj.content}</p>
                                 </section>
                             </div>
                         </div>   
