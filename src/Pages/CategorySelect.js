6d96c33364834799b23fb0af460c3e6e2b7f4313
Jon Ekse <jrekse@gmail.com>
2021-10-11 08:28:31 -0700

    updated category data, and updated the links to these scripts

--- src/Pages/CategorySelect.js
+++ src/Pages/CategorySelect.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
-import VSSM from '../assets/images/vssbannerhead.jpeg';
-import VBann from '../assets/images/verles.jpg';
 import BlackH from "../assets/images/black_hills-300X600.jpg"
-import Data from '../assets/js/CategoryData';
-import Data2 from '../assets/js/CategoryData2';
-import Data3 from '../assets/js/CategoryData3';
 import '../assets/css/CatSelect.css';
 
 class Catselect extends Component {
@@ -15,39 +10,20 @@ class Catselect extends Component {
       return (
         <div className="CSBody">
 
-            <header className="CSHead">
-                <img className="vssmast" alt='VSS Masthead' src={VSSM}/>
-                    <br/>
-                <a href="https://www.verles.com/">
-                    <img className="VBanner" alt='VerlesBanner' src={VBann}/>
-                </a>
-            </header>
-
             <section className='CSMain'>
 
                 <section className='categoryTable'>
                     <table id="simple-board">
                         <tbody>
                             <tr id="row0">
-                                {Data.map((post, index) => {
-                                    return(
-                                        <td id="cell0-0" className={index}>{post.title}</td>
-                                    )
-                                })}
+                                <td id="cell0-0"><a href='https://css-tricks.com/creating-the-perfect-commit-in-git/'>Auctioneers</a></td>
                             </tr>
                             <tr id="row1">
-                                {Data2.map((post, index) => {
-                                    return(
-                                        <td id="cell1-0" className={index + 4}>{post.title}</td>
-                                    )
-                                })}
+                                <td id="cell1-0" >Apparel / Accessories</td>
+                             
                             </tr>
                             <tr id="row2">
-                                {Data3.map((post, index) => {
-                                    return(
-                                        <td id="cell2-0" className={index + 8}>{post.title}</td>
-                                    )
-                                })}
+                                <td id="cell2-0" >Boats</td>
                             </tr>
                         </tbody>                          
                     </table>
