75a8d4f5abe86f1af2135c8dec5f1b1a61a15b34
Jon Ekse <jrekse@gmail.com>
2021-10-18 09:19:01 -0700

    combined into 2 rows, added missing link

--- src/Pages/CategorySelect.js
+++ src/Pages/CategorySelect.js
@@ -15,7 +15,7 @@ function Catselect(props){
                             <tr>
                                 <td><a href='#Air' onClick={() => props.handlePageChange("Air")}>Air Freight</a></td> 
                                 <td><a href='#Apparel' onClick={() => props.handlePageChange("Apparel")}>Apparel</a></td>
-                                <td><a href='#Boats' onClick={() => props.handlePageChange("Boats")}>Boats</a></td>
+                                <td><a href='#Auction' onClick={() => props.handlePageChange("Auction")}>Auctioneers</a></td>
                                 <td><a href='#FishingResorts' onClick={() => props.handlePageChange("FishingResorts")}>Fishing Resorts</a></td>
                                 <td><a href='#Food' onClick={() => props.handlePageChange("Food")}>Food</a></td>
                                 <td><a href='#Furniture' onClick={() => props.handlePageChange("Furniture")}>Furniture</a></td>
@@ -24,6 +24,12 @@ function Catselect(props){
                                 <td><a href='#PowerEquip' onClick={() => props.handlePageChange("Power Equipment")}>Power Equipment</a></td>
                                 <td><a href='#HuntingEducation' onClick={() => props.handlePageChange("Hunting Education")}>Hunting Education</a></td>
                                 <td><a href='#SportFishing' onClick={() => props.handlePageChange("Sport Fishing")}>Sport Fishing</a></td>
+                                <td><a href='#FirearmsAmmo' onClick={() => props.handlePageChange("FirearmsAmmo")}>Firearms / Ammo</a></td>
+                                <td><a href='#Firearm Cleaning' onClick={() => props.handlePageChange("Firearm Cleaning")}>Firearm Cleaning</a></td>
+                                <td><a href='#Fishing Equipment' onClick={() => props.handlePageChange("Fishing Equipment")}>Fishing Equipment</a></td>
+                                <td><a href='#Holsters' onClick={() => props.handlePageChange("Holsters")}>Holsters</a></td>
+                                <td><a href='#Hunting' onClick={() => props.handlePageChange("Hunting")}>Hunting</a></td>
+                                <td><a href='#Insurance' onClick={() => props.handlePageChange("Insurance")}>Insurance</a></td>
                             </tr>
                             <tr>
                                 <td><a href='#Construction' onClick={() => props.handlePageChange("Construction")}>Construction</a></td>
@@ -37,14 +43,7 @@ function Catselect(props){
                                 <td><a href='#Resturaunts' onClick={() => props.handlePageChange("Resturaunts")}>Resturaunts</a></td>
                                 <td><a href='#Realty' onClick={() => props.handlePageChange("Realty")}>Realty</a></td>
                                 <td><a href='#Tourism' onClick={() => props.handlePageChange("Tourism")}>Tourism</a></td>
-                            </tr>
-                            <tr>
-                                <td><a href='#FirearmsAmmo' onClick={() => props.handlePageChange("FirearmsAmmo")}>Firearms / Ammo</a></td>
-                                <td><a href='#Firearm Cleaning' onClick={() => props.handlePageChange("Firearm Cleaning")}>Firearm Cleaning</a></td>
-                                <td><a href='#Fishing Equipment' onClick={() => props.handlePageChange("Fishing Equipment")}>Fishing Equipment</a></td>
-                                <td><a href='#Holsters' onClick={() => props.handlePageChange("Holsters")}>Holsters</a></td>
-                                <td><a href='#Hunting' onClick={() => props.handlePageChange("Hunting")}>Hunting</a></td>
-                                <td><a href='#Insurance' onClick={() => props.handlePageChange("Insurance")}>Insurance</a></td>
+                                <td><a href='#Boats' className='centered' onClick={() => props.handlePageChange("Boats")}>Boats</a></td>
                                 <td><a href='#Motorcycles' onClick={() => props.handlePageChange("Motorcycles")}>Motorcycles</a></td>
                                 <td><a href='#NonProfits' onClick={() => props.handlePageChange("NonProfit")}>NonProfits</a></td>
                                 <td><a href='#RVs' onClick={() => props.handlePageChange("RV's")}>RV's</a></td>
